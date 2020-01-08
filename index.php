40904221267b3887c436d37d04b091c1181d69cf
Ken Ingram <kingram@ZEUSPRIME.attlocal.net>
2020-01-08 10:16:18 -0800

    Work out bootstrap integration.

--- index.php
+++ index.php
@@ -1,22 +1,41 @@
 <!doctype html>
-<html>
+<html lang="en">
 	<head>
 		<title></title>
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<script src="js/base.js"></script>
+		<link rel="stylesheet" href="css/default.css">
 	</head>
 	<body>
-		<form id="login">
-		<label for="fname">First Name:</label>	<input id="fname" type="text">
-		<label for="mname">Middle Name:</label>	<input id="mname" type="text">
-		<label for="lname">Last Name:</label>	<input id="lname" type="text">
-		<label for="email">Email:</label>	<input id="email" type="text">
-		<label for="passwd">Password:</label>	<input id="passwd" type="password">
-		<button onclick="formSubmit()">Submit</button>
-		</form>
-		
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-5 mol-md-4 box">XS column 1</div>
+				<div class="col-xs-5 offset-xs-2 col-md-8 box offset-md-0">XS column 2</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6 box">SM column 1</div>
+				<div class="col-sm-6 box">SM column 2</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 box">LG column 1</div>
+				<div class="col-lg-6 box">LG column 2</div>
+			</div>
+			<div class="col-lg-12 box">
+				<form id="login">
+				<label for="fname">First Name:</label>	<input id="fname" type="text">
+				<label for="mname">Middle Name:</label>	<input id="mname" type="text">
+				<label for="lname">Last Name:</label>	<input id="lname" type="text">
+				<label for="email">Email:</label>	<input id="email" type="text">
+				<label for="passwd">Password:</label>	<input id="passwd" type="password">
+				<button onclick="formSubmit()">Submit</button>
+				</form>	
+			</div>
+		</div>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="js/base.js"></script>
 	</body>	
 </html>
